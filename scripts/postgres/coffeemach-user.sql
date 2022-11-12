7d0631db064186b519f5db4c7b19fe0b9ef79355
Sebastian Garcia <sebastian.garcia@wearemo.com>
2022-11-12 13:04:58 -0500

    Changes name and passworld of the database. Created succesfully

--- scripts/postgres/coffeemach-user.sql
+++ scripts/postgres/coffeemach-user.sql
@@ -1,6 +1,6 @@
-create user cofmachu with password 'cofmachpwd';
-create database coffeemachine owner cofmachu;
-grant connect on database coffeemachine to cofmachu;
+create user avengers with password 'cofmachpwd';
+create database coffeemachine_avengers owner avengers;
+grant connect on database coffeemachine_avengers to avengers;
 
 -- Connection
 -- psql -d coffeemachine -U p09713_1_2
